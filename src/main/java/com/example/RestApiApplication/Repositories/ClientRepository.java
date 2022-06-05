eaa56a9cf1f157ae39d16dd67ac636f92ce47ea3
kuzkovandrey <kuzkovandrey@gmail.com>
2022-06-05 15:06:22 +0300

    fg

--- src/main/java/com/example/RestApiApplication/Repositories/ClientRepository.java
+++ src/main/java/com/example/RestApiApplication/Repositories/ClientRepository.java
@@ -3,10 +3,8 @@ package com.example.RestApiApplication.Repositories;
 import java.util.List;
 import com.example.RestApiApplication.Entities.Client;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ClientRepository extends CrudRepository<Client, Long> { 
+public interface ClientRepository extends CrudRepository<Client, Long> {
   Client findByNameAndEmail(String name, String email);
   List<Client> findAll();
 }
--- src/main/java/com/example/RestApiApplication/Repositories/PriceListRepository.java
+++ src/main/java/com/example/RestApiApplication/Repositories/PriceListRepository.java
@@ -3,9 +3,7 @@ package com.example.RestApiApplication.Repositories;
 import java.util.List;
 import com.example.RestApiApplication.Entities.PriceList;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PriceListRepository extends CrudRepository<PriceList, Long> {
   PriceList findByDescription(String name);
   List<PriceList> findAll();
