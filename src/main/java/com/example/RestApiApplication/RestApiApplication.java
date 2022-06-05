7285111333496b52d893a0eb3503240e7ffbe061
kuzkovandrey <kuzkovandrey@gmail.com>
2022-06-05 14:15:05 +0300

    Fix main class

--- src/main/java/com/example/RestApiApplication/RestApiApplication.java
+++ src/main/java/com/example/RestApiApplication/RestApiApplication.java
@@ -3,11 +3,12 @@ package com.example.RestApiApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.RestApiApplication.*"} , exclude = JpaRepositoriesAutoConfiguration.class)
 public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
