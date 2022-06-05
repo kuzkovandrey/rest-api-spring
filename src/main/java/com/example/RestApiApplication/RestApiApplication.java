78ead3084715e6959c43f9b3eee0ba38a77fa7eb
kuzkovandrey <kuzkovandrey@gmail.com>
2022-06-05 14:21:58 +0300

    f

--- src/main/java/com/example/RestApiApplication/RestApiApplication.java
+++ src/main/java/com/example/RestApiApplication/RestApiApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
-@EnableAutoConfiguration
-@SpringBootApplication(scanBasePackages = {"com.example.RestApiApplication.*"} , exclude = JpaRepositoriesAutoConfiguration.class)
+//@ComponentScan
+//@EnableAutoConfiguration
+@SpringBootApplication
 public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
