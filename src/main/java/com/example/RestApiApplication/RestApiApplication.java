24aa36524cac2016df5fbbd3f327ccdeefe39cc6
kuzkovandrey <kuzkovandrey@gmail.com>
2022-06-05 13:09:55 +0300

    removed annotations

--- src/main/java/com/example/RestApiApplication/RestApiApplication.java
+++ src/main/java/com/example/RestApiApplication/RestApiApplication.java
@@ -2,14 +2,8 @@ package com.example.RestApiApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.example.RestApiApplication.*"})
-@ComponentScan(basePackages = { "com.example.RestApiApplication.*" })
-@EntityScan(basePackages = {"com.example.RestApiApplication.Entities"})
 public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
