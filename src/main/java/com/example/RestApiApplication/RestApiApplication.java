995823aa78ca914894e1455c7068c71984b3b016
kuzkovandrey <kuzkovandrey@gmail.com>
2022-06-05 12:23:55 +0300

    test

--- src/main/java/com/example/RestApiApplication/RestApiApplication.java
+++ src/main/java/com/example/RestApiApplication/RestApiApplication.java
@@ -3,10 +3,7 @@ package com.example.RestApiApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-	"com.example.RestApiApplication.Repositories",
-	"com.example.RestApiApplication.Services"
-})
+@SpringBootApplication
 public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
